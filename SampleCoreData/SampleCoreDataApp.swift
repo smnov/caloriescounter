06dc819876eebd4c715b5b8080e8268fc147f5ab
aleksandr semenov <aleksandr.semenovsky@gmail.com>
2024-01-15 21:11:43 +0800

    Initial Commit

---
+++ SampleCoreData/SampleCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleCoreDataApp.swift
+//  SampleCoreData
+//
+//  Created by Александр Семенов on 15.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct SampleCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
